69be0bfd790d33d01b1c993fe091c0bcb64748a6
098orin <098orin0102@gmail.com>
2026-01-02 19:47:25 +0900

    Organize app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,27 @@
+use anyhow::Result;
 use std::sync::Arc;
-use winit::application::ApplicationHandler;
-use winit::event::WindowEvent;
-use winit::event_loop::ActiveEventLoop;
-use winit::window::{Window, WindowId};
+use winit::{
+    application::ApplicationHandler,
+    event::{KeyEvent, WindowEvent},
+    event_loop::ActiveEventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::Window,
+};
 
 pub struct State {
     pub window: Arc<Window>,
+}
+
+impl State {
+    pub async fn new(window: Arc<Window>) -> Result<Self> {
+        Ok(Self { window })
+    }
+
+    pub fn resize(&mut self, _width: u32, _height: u32) {}
+
+    pub fn render(&mut self) {
+        self.window.request_redraw();
+    }
 }
 
 pub struct App {
@@ -25,9 +41,7 @@ impl ApplicationHandler<State> for App {
                 .create_window(Window::default_attributes())
                 .unwrap(),
         );
-        let state = State {
-            window: window.clone(),
-        };
+        let state = pollster::block_on(State::new(window)).unwrap();
         self.state = Some(state);
 
         // 初回描画
@@ -38,9 +52,34 @@ impl ApplicationHandler<State> for App {
 
     fn window_event(
         &mut self,
-        _event_loop: &ActiveEventLoop,
-        _window_id: WindowId,
-        _event: WindowEvent,
+        event_loop: &ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: WindowEvent,
     ) {
+        let state = match &mut self.state {
+            Some(canvas) => canvas,
+            None => return,
+        };
+
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => state.resize(size.width, size.height),
+            WindowEvent::RedrawRequested => {
+                state.render();
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(code),
+                        state: key_state,
+                        ..
+                    },
+                ..
+            } => match (code, key_state.is_pressed()) {
+                (KeyCode::Escape, true) => event_loop.exit(),
+                _ => {}
+            },
+            _ => {}
+        }
     }
 }
