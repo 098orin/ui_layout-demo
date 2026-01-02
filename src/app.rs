ef394d64e58f54035871ec938f3f530cbb728216
098orin <098orin0102@gmail.com>
2026-01-02 19:30:56 +0900

    Create app.rs

---
+++ src/app.rs
@@ -0,0 +1,46 @@
+use std::sync::Arc;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::ActiveEventLoop;
+use winit::window::{Window, WindowId};
+
+pub struct State {
+    pub window: Arc<Window>,
+}
+
+pub struct App {
+    state: Option<State>,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self { state: None }
+    }
+}
+
+impl ApplicationHandler<State> for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+        let state = State {
+            window: window.clone(),
+        };
+        self.state = Some(state);
+
+        // 初回描画
+        if let Some(state) = &mut self.state {
+            state.window.request_redraw();
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        _event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        _event: WindowEvent,
+    ) {
+    }
+}
